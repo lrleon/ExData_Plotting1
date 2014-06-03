c0e1929c59a3e528fef7de56de9b721209bd4503
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2014-06-03 18:13:19 -0430

    Ready for submission

--- load-data.R
+++ load-data.R
@@ -1,6 +1,11 @@
 
 #
 # load data into a data frame
+#
+# return a data frame with the loaded data and set in the global
+# environment the variable lr.data. In this way we save reloading and
+# processing of the data
+#
 load.data <- function() {
 
     if (exists("lr.data"))
@@ -20,12 +25,17 @@ load.data <- function() {
     message("Loading and processing data. Please wait ...")
     data <- read.table(filename, header=T, sep=";", nrows=2075259,
                        colClasses = "character")
-        
-    data$Date <- as.Date(data$Date, "%d/%m/%Y") 
-    data <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02", ]
+
+                                 # convert date and time to adequate format
+    data$Date <- as.Date(data$Date, "%d/%m/%Y")
+                                 # filter the required dates
+    data <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02",]
+                                 # and now convert the time
     data$Time <- strptime(data$Time, "%T")
 
-    lr.data <<- data
+    lr.data <<- data   # put lr.data in global environment. In this
+                       # way, the data will remains loaded without need
+                       # of to repeat its loading from the file
 
     message("Necessary data is in lr.data data frame")
     message("NOTE: prefix lr is used in order to mitigate name space pollution")
