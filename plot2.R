e90092237a8be3a957f6db69d34a292e242127b6
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2014-06-03 10:30:41 -0430

    Ready plot2

---
+++ plot2.R
@@ -0,0 +1,19 @@
+source("load-data.R")
+
+plot2 <- function() {
+
+    load.data()
+
+    plot(1:2880, lr.data$Global_active_power, type="l", axes=F, xlab="",
+         ylab="Global Active Power (kilowatts)")
+    axis(1, at=c(0, 1440, 2880), labels=c("Thu","Fri","Sat"))
+    axis(2, at=c(0, 2, 4, 6))
+    box()
+}
+
+plot2()
+
+png(filename="plot2.png", width=480, height=480)
+plot1()
+dev.off()
+message("histogram was saved in plot2.png file")
